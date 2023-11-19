71af62a3a97ddc41a286474d295e26d9c2cc09d5
ArgusWrites <baylonfrancis4work@gmail.com>
2023-11-19 20:30:00 +0800

    Landing Page

--- app/src/main/java/org/tensorflow/lite/examples/imageclassification/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/imageclassification/MainActivity.java
@@ -25,7 +25,6 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private TextToSpeech tts;
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private ActivityMainBinding activityMainBinding;
     private SpeechRecognizer stt;
     private Handler handler;
@@ -36,117 +35,5 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
-        checkPermission();
-
-        handler = new Handler(Looper.getMainLooper());
-
-        // Initialize TextToSpeech
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    // Set language to the default locale
-                    int result = tts.setLanguage(Locale.ENGLISH);
-                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Toast.makeText(MainActivity.this, "Text-to-Speech not supported on this device.", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // Use the initialized tts variable, not the finalTts
-                        tts.speak("Shall we start?", TextToSpeech.QUEUE_FLUSH, null, "utteranceId");
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Initialization failed.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        // Initialize SpeechRecognizer
-        stt = SpeechRecognizer.createSpeechRecognizer(this);
-        stt.setRecognitionListener(new MyRecognitionListener());
-    }
-
-    private void checkPermission() {
-        // Check for the record audio permission
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
-                    REQUEST_RECORD_AUDIO_PERMISSION);
-        }
-    }
-
-    /** @noinspection deprecation*/
-    public void startSpeechRecognition(View view) {
-        // Start speech recognition
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
-
-        try {
-            startActivityForResult(intent, REQUEST_RECORD_AUDIO_PERMISSION);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, "Speech recognition not supported on this device.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private class MyRecognitionListener implements RecognitionListener {
-        @Override
-        public void onReadyForSpeech(Bundle bundle) {
-
-        }
-
-        @Override
-        public void onBeginningOfSpeech() {
-
-        }
-
-        @Override
-        public void onRmsChanged(float v) {
-
-        }
-
-        @Override
-        public void onBufferReceived(byte[] bytes) {
-
-        }
-
-        @Override
-        public void onEndOfSpeech() {
-
-        }
-
-        @Override
-        public void onError(int i) {
-
-        }
-
-        @Override
-        public void onResults(Bundle bundle) {
-
-        }
-
-        @Override
-        public void onPartialResults(Bundle bundle) {
-
-        }
-
-        @Override
-        public void onEvent(int i, Bundle bundle) {
-
-        }
-        // Implement necessary methods for RecognitionListener
-        // You can override onResults, onError, onPartialResults, etc.
-        // See the documentation for SpeechRecognizer and RecognitionListener for details.
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (stt != null) {
-            stt.destroy();
-        }
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
     }
 }
